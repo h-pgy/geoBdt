3dd1bc351935fad342e371d422fd1d4f9f2dcb77
h-pgy <hpougy@gmail.com>
2020-10-27 19:22:32 -0300

    created helper for building response

---
+++ BdtApi/helpers.py
@@ -0,0 +1,8 @@
+
+
+def build_response(label, description, value):
+    """Simple function for making code leaner"""
+
+    return {'label' : label,
+            'description' : description,
+            'value' : value}
